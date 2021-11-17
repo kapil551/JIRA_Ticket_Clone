d3091b56c957ab2674a971f2ead163994477d924
kapil551 <59477738+kapil551@users.noreply.github.com>
2021-11-17 12:08:26 +0530

    Create Ticket - added some JavaScript logic to create a new ticket

--- script.js
+++ script.js
@@ -53,14 +53,26 @@ let createNewTicket = () => {
     console.log("creating a new ticket...");
 
     // create a new div element
-    let newDivForTicket = document.createElement("div");
-    newDivForTicket.setAttribute("class", "ticket-cont");
-    newDivForTicket.innerHTML = `
-        <div class="ticket-color"></div>
-            <div class="ticket-id">#sample_id</div>
-            <div class="task-area">
+    /*
+        THis is the div that, I want to create.
+        <div class="ticket-cont h-48 w-64 bg-gray-200">
+            <div class="ticket-color h-4 bg-pink-500"></div>
+            <div class="ticket-id h-8 p-2">#sample_id</div>
+            <div class="task-area h-5/6 p-2">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit 
                 Ad ea, recusandae vitae perspiciatis tempora accusamus.
+            </div>
+
+        </div>
+     */
+    let newDivForTicket = document.createElement("div");
+    newDivForTicket.setAttribute("class", "ticket-cont h-48 w-64 bg-gray-200");
+    newDivForTicket.innerHTML = `
+        <div class="ticket-color h-4 bg-pink-500"></div>
+        <div class="ticket-id h-8 p-2">#sample_id</div>
+        <div class="task-area h-5/6 p-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit 
+            Ad ea, recusandae vitae perspiciatis tempora accusamus.
         </div>
     `
 
