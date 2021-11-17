1304b7bb754d8cfb12e6690195d44198eb110822
kapil551 <59477738+kapil551@users.noreply.github.com>
2021-11-17 10:17:51 +0530

    JavaScript Logic - logic to show and hide modal container

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("Hello from JavaScript")
+let addBtn = document.querySelector(".add-btn");
+// console.log(addBtn);
+let modalContainer = document.querySelector(".modal-cont");
+// console.log(modalContainer);
+
+let addFlag = false;
+
+// when the user clicks on the add button then show the modal container,
+// and when the user again clicks on the add button then again hide the modal container.
+const ModalContainerHandler = () => {
+    // Display modal
+    // Generate ticket
+
+    // addFlag, true ==> display modal container
+    // addFlag, false ==> make the modal container disappear
+    addFlag = !addFlag;
+
+    if(addFlag === true) {
+        // display modal container
+        modalContainer.style.display = "flex";
+    }
+    else {
+        // make the modal container disappear
+        modalContainer.style.display = "none";
+    }
+
+
+}
+
+// Task - When the use clicks on the addBtn, then the modal container should
+// appear, otherwise it should remain disapear.
+addBtn.addEventListener("click", ModalContainerHandler);
